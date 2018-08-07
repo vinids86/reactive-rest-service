2cf3d0eb0a45746c3863387d50643ff22cdf2f97
Vinicius.saraiva Acc <vinicius.saraiva.acc@accurate.com.br>
2018-08-07 15:43:58 -0300

    Make it better

---
+++ src/main/java/br/com/saraiva/Application.java
@@ -0,0 +1,16 @@
+package br.com.saraiva;
+
+import br.com.saraiva.hello.GreetingWebClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        final GreetingWebClient client = new GreetingWebClient();
+        System.out.println(client.getResult());
+    }
+}
